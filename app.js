faf1d54e5038da9a302c411cf95ecb0be97c0a2d
FreeWill <wjett18@gmail.com>
2020-09-06 22:11:39 -0700

    fixed port

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ app.use(cors());
 var campsites = require("./Campsites_API_v1.json");
 let facs = campsites["RECDATA"];
 
+var port = process.env.PORT || 8080;
+
 app.get("/facilities", (req, res, next) => {
     res.json(facs);
 });
 
-app.listen(process.env.PORT ||3000, () => {
- console.log("Server running on port 3000");
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
-
