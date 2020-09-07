689d3f5b3d533c98d1e6e30a84905dc1c23fe3f9
FreeWill <wjett18@gmail.com>
2020-09-06 21:58:27 -0700

    campsite service initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var cors = require('cors');
+app.use(cors());
+
+var campsites = require("./Campsites_API_v1.json");
+let facs = campsites["RECDATA"];
+
+app.get("/facilities", (req, res, next) => {
+    res.json(facs);
+});
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
