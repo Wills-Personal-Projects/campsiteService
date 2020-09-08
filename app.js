9e3b5b578452bd43e961b3c974ffc9e9440180ec
FreeWill <wjett18@gmail.com>
2020-09-07 19:02:41 -0700

    deploy it again again again!

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get("/getUserCampsites", function (req, res) {
 
 app.post('/postCampsite', (req, res) => {
     userSites["Campsites"].push("new site added");
-    return res.send(req.body);
+    return res.send(req);
 });
    
 app.put('/', (req, res) => {
